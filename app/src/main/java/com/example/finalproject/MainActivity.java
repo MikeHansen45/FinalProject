981d109c611a890b0e4fc4f83a317c8f77606fcc
Michael Hansen <M_Hansen45@hotmail.com>
2020-11-15 11:32:33 -0500

    Added connection to ticket master and basic parsing started building the class to hold ticketmaster info

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs = null;
+    Button toTicket;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toTicket = findViewById(R.id.toTicket);
+        Intent goToTicket = new Intent(this,TicketMasterActivity.class);
+
+        toTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToTicket);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/finalproject/TicketMasterActivity.java
@@ -0,0 +1,110 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+
+public class TicketMasterActivity extends AppCompatActivity {
+    private ImageButton search_IB;
+    private EditText cityName_ET;
+    private EditText radius_ET;
+    private ProgressBar search_PB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_master);
+
+        //Assigns all the xml item ids to their java variables
+        search_IB = findViewById(R.id.search_IB);
+        cityName_ET = findViewById(R.id.cityName_ET);
+        radius_ET = findViewById(R.id.radius_ET);
+        search_PB = findViewById(R.id.searchPB);
+
+        search_IB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //apikey=ZeH2TvddeHJytkYTsWA4F3GQ9gIWaVZB
+                // https://app.ticketmaster.com/discovery/v2/events.json?apikey=ZeH2TvddeHJytkYTsWA4F3GQ9gIWaVZB&city=toronto&radius=100
+
+                search_PB.setVisibility(View.VISIBLE);
+                SearchTktMstr req = new SearchTktMstr();
+                req.execute("https://app.ticketmaster.com/discovery/v2/events.json?apikey=ZeH2TvddeHJytkYTsWA4F3GQ9gIWaVZB&city=toronto&radius=100");
+            }
+        });
+
+
+
+    }
+    private class SearchTktMstr extends AsyncTask<String,Integer,String>{
+        @Override
+        protected String doInBackground(String... args) {
+            String JSONresult;
+            String line = null;
+            String name;
+            try {
+                URL url = new URL(args[0]);
+                HttpURLConnection jsonUrlConnection = (HttpURLConnection) url.openConnection();
+                InputStream responseJson = jsonUrlConnection.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(responseJson, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+                while((line = reader.readLine()) !=null){
+                    sb.append(line +"\n");
+//                    Log.d("LINE", line);
+                }
+                JSONresult = sb.toString();
+                JSONObject tktMstJSON = new JSONObject(JSONresult);
+                JSONObject embeded = tktMstJSON.getJSONObject("_embedded");
+                JSONArray events = embeded.getJSONArray("events");
+
+                Log.d("PLEASE FUCKING WORK", "WOROROROOROR");
+                   for(int i =0; i<events.length();i++){
+                    JSONObject obj = events.getJSONObject(i);
+                    name = obj.getString("name");
+                    Log.d("PLEASE BE THE NAME",name);
+
+                }
+
+
+
+
+
+            }
+            catch (Exception e){
+                Log.d("IN CATCH", "FUCK");
+            }
+            finally {
+                // close connection
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
