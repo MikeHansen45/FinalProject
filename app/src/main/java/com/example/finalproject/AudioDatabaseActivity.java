668d99bfd3d2716bf0698d1409ec8bb4f7841636
ben <dalt0044@algonquinlive.com>
2020-11-15 19:29:44 -0500

    created and adding audio database layout and activity file

---
+++ app/src/main/java/com/example/finalproject/AudioDatabaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AudioDatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_database2);
+    }
+}
