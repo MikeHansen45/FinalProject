11c2caed1892d6b9cd552a140a716b52df8ab06f
ben <dalt0044@algonquinlive.com>
2020-11-26 17:26:21 -0500

    working on milestone 2...

--- app/src/main/java/com/example/finalproject/AudioDatabaseActivity.java
+++ app/src/main/java/com/example/finalproject/AudioDatabaseActivity.java
@@ -3,7 +3,11 @@ package com.example.finalproject;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -12,12 +16,19 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class AudioDatabaseActivity extends AppCompatActivity {
 
     ListView listView;
     Button button;
     ProgressBar progressBar;
     EditText editText;
+
+    ArrayList elements = new ArrayList<>();
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +57,32 @@ public class AudioDatabaseActivity extends AppCompatActivity {
 
             Snackbar snackbar = Snackbar.make(listView, "message", Snackbar.LENGTH_LONG);
             snackbar.show();
+
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString(TEXT, editText.getText().toString());
+            editor.apply();
+
         });
 
+    }
+
+    class AudioDBAdapter extends BaseAdapter {
+        public int getCount() {
+            return elements.size();
+        }
+
+        public String getItem(int position) {
+            return elements.get(position).toString();
+        }
+
+        public long getItemId(int position) {
+            return (long)position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+        }
     }
 }
